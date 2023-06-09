003b6034d2b5267d5a674a7e487fd2a74db7b37c
N.G. Cartier <91268563+NGCartier@users.noreply.github.com>
2023-06-09 18:41:25 +0200

    Delete General_func_Ref_closehess_v01 directory
    Delete old version

--- General_func_Ref_closehess_v01/fides-cpp-main/include/fides/fides.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef FIDES_FIDES_HPP
-#define FIDES_FIDES_HPP
-
-namespace fides {} // namespace fides
-
-#endif // FIDES_FIDES_HPP
--- General_func_Ref_closehess_v01/fides-cpp-main/src/fides.cpp
+++
@@ -1,3 +0,0 @@
-#include "../include/fides/fides.hpp"
-
-namespace fides {} // namespace fides
