1694c23eedd4a72ce81db745a509ef5c5c9345b1
N.G. Cartier <91268563+NGCartier@users.noreply.github.com>
2024-01-31 09:49:53 +0100

    Update numerical_deriv.hpp

--- SCF-RDMFT_package/lib/numerical_deriv/numerical_deriv.hpp
+++ SCF-RDMFT_package/lib/numerical_deriv/numerical_deriv.hpp
@@ -4,5 +4,4 @@ class Functional; class RDM1;
 
 Eigen::VectorXd grad_func(Functional* func, RDM1* gamma, bool do_n=true, bool do_no=true, double epsi=1e-6);
 Eigen::MatrixXd hess_func(Functional* func, RDM1* gamma, bool do_n=true, bool do_no=true, double epsi=1e-6);
-Eigen::MatrixXd hess_aux (Functional* func, RDM1* gamma, bool do_n=true, bool do_no=true, double epsi=5e-5);
 #endif
